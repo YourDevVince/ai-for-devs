8a051841dd938f4ce53659205f2eacfc8f53556d
YourDevVince <vince.dev.help@gmail.com>
2026-01-31 17:00:34 -0600

    intitial commit

---
+++ server.mjs
@@ -0,0 +1,26 @@
+import express from 'express';
+import { ChatOllama } from '@langchain/ollama';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const chatModel = new ChatOllama({
+  model: 'llama3.2',
+});
+
+app.post('/', async (request, response) => {
+  const body = request.body;
+  const stream = await chatModel.stream(body.question);
+
+  for await (const chunk of stream) {
+    response.write(chunk.content);
+  }
+  response.end();
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port 8000');
+});
